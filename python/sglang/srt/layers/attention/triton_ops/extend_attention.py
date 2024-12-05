20efc43be7235882163778ea187a79d232cec70a
Zhang, Liangang <liangang.zhang@intel.com>
2024-12-04 22:59:29 -0800

    Get on-par prefill latency with xetla

--- python/sglang/srt/layers/attention/triton_ops/extend_attention.py
+++ python/sglang/srt/layers/attention/triton_ops/extend_attention.py
@@ -40,7 +40,237 @@ def tanh(x):
     # Tanh is just a scaled sigmoid
     return 2 * tl.sigmoid(2 * x) - 1
 
+@triton.jit
+def _attn_fwd_inner(acc, deno, e_max, q, k_block_ptr, v_block_ptr, start_m, sm_scale, logit_cap, cur_block_m_end, mask_m, #
+                    BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr, BLOCK_N: tl.constexpr,
+                    STAGE: tl.constexpr, offs_m: tl.constexpr, offs_n: tl.constexpr):
+        # range of values handled by this stage
+    if STAGE == 1:
+        lo, hi = 0, start_m * BLOCK_M
+    elif STAGE == 2:
+        lo, hi = start_m * BLOCK_M, (start_m + 1) * BLOCK_M
+        lo = tl.multiple_of(lo, BLOCK_M)
+        hi = cur_block_m_end
+    # causal = False
+    else:
+        lo, hi = 0, cur_block_m_end
+    k_block_ptr = tl.advance(k_block_ptr, (0, lo))
+    v_block_ptr = tl.advance(v_block_ptr, (lo, 0))
 
+
+    for start_n in range(lo, hi, BLOCK_N):
+        start_n = tl.multiple_of(start_n, BLOCK_N)
+        mask_n = (start_n + offs_n) < cur_block_m_end
+
+        # k:[BLOCK_N, 1, BLOCK_DMODEL]
+        k = tl.load(k_block_ptr, boundary_check=(0, 1))
+        #k = k.trans()
+
+        qk = tl.dot(q, k, out_dtype=tl.float32)
+        #if BLOCK_DPE > 0:
+        #    offs_kpe = (
+        #        (cur_seq_extend_start_contiguous + start_n + offs_n[None, :])
+        #        * stride_kbs
+        #        + cur_kv_head * stride_kh
+        #        + offs_dpe[:, None]
+        #    )
+        #    kpe = tl.load(
+        #        K_Extend + offs_kpe,
+        #        mask=mask_n[None, :],
+        #        other=0.0,
+        #    )
+        #    qk += tl.dot(qpe, kpe)
+
+        qk *= sm_scale
+
+        if logit_cap > 0:
+            qk = logit_cap * tanh(qk / logit_cap)
+
+        if STAGE == 2:
+            #mask_causual = (cur_block_m * BLOCK_M + offs_m[:, None]) >= (
+            #    start_n + offs_n[None, :]
+            #)
+            #mask_causual &= mask_m[:, None] & mask_n[None, :]
+            mask = offs_m[:, None] >= (start_n + offs_n[None, :])
+            mask &= mask_m[:, None] & mask_n[None, :]
+            qk = tl.where(mask, qk, float("-inf"))
+            
+
+        n_e_max = tl.maximum(tl.max(qk, 1), e_max)
+        re_scale = tl.exp(e_max - n_e_max)
+        p = tl.exp(qk - n_e_max[:, None])
+        deno = deno * re_scale + tl.sum(p, 1)
+        v = tl.load(v_block_ptr, boundary_check=(0, 1))
+
+        p = p.to(v.dtype)
+        acc = acc * re_scale[:, None] + tl.dot(p, v)
+
+        e_max = n_e_max
+
+        v_block_ptr = tl.advance(v_block_ptr, (BLOCK_N, 0))
+        k_block_ptr = tl.advance(k_block_ptr, (0, BLOCK_N))
+    return acc, deno, e_max
+
+@triton.jit
+def _fwd_kernel2(
+    Q_Extend,
+    K_Extend,
+    V_Extend,
+    O_Extend,
+    K_Buffer,
+    V_Buffer,
+    Req_to_tokens,
+    B_req_idx,
+    B_Seq_Len,
+    B_Start_Loc_Extend,
+    B_Seq_Len_Extend,
+    sm_scale,
+    q_head_num,
+    stride_qbs,
+    stride_qh,
+    stride_kbs,
+    stride_kh,
+    stride_vbs,
+    stride_vh,
+    stride_obs,
+    stride_oh,
+    stride_buf_kbs,
+    stride_buf_kh,
+    stride_buf_vbs,
+    stride_buf_vh,
+    stride_req_to_tokens_b,
+    logit_cap: tl.constexpr,
+    Lq: tl.constexpr,
+    Lv: tl.constexpr,
+    kv_group_num: tl.constexpr,
+    use_block_ptr: tl.constexpr,
+    BLOCK_DMODEL: tl.constexpr,
+    BLOCK_DPE: tl.constexpr,
+    BLOCK_DV: tl.constexpr,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+):
+    cur_seq = tl.program_id(2)
+    cur_head = tl.program_id(0)
+    cur_block_m = tl.program_id(1)
+    cur_kv_head = cur_head // kv_group_num
+    kv_head_num = q_head_num // kv_group_num
+
+    cur_seq_len = tl.load(B_Seq_Len + cur_seq)
+    cur_seq_len_extend = tl.load(B_Seq_Len_Extend + cur_seq)
+    cur_seq_len_prefix = cur_seq_len - cur_seq_len_extend
+    cur_seq_prefix_start_in_loc = 0
+    cur_seq_extend_start_contiguous = tl.load(B_Start_Loc_Extend + cur_seq)
+    cur_batch_req_idx = tl.load(B_req_idx + cur_seq)
+
+    offs_d = tl.arange(0, BLOCK_DMODEL)
+    offs_dv = tl.arange(0, BLOCK_DV)
+    offs_m = tl.arange(0, BLOCK_M)
+    mask_m = (cur_block_m * BLOCK_M + offs_m) < cur_seq_len_extend
+
+    mask_d = offs_d < Lq
+    mask_dv = offs_dv < Lv
+
+    # Ragged inputs: Q:[S, H, D]
+    offs_q = cur_seq_extend_start_contiguous * stride_qbs + cur_head * stride_qh
+    # the offset to the current seq
+    q_block_ptr = tl.make_block_ptr(
+        base=Q_Extend + offs_q,
+        shape=(cur_seq_len_extend, Lq),
+        strides=(stride_qbs, 1),
+        offsets=(cur_block_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, BLOCK_DMODEL),
+        order=(1, 0),
+    )
+    # q:[BLOCK_M, 1, BLOCK_DMODEL]
+    q = tl.load(q_block_ptr, boundary_check=(0, 1))
+    #q = tl.reshape(q, (BLOCK_M, BLOCK_DMODEL), can_reorder=False)
+
+    if BLOCK_DPE > 0:
+        offs_dpe = BLOCK_DMODEL + tl.arange(0, BLOCK_DPE)
+        offs_qpe = (
+            (cur_seq_extend_start_contiguous + cur_block_m * BLOCK_M + offs_m[:, None])
+            * stride_qbs
+            + cur_head * stride_qh
+            + offs_dpe[None, :]
+        )
+        qpe = tl.load(Q_Extend + offs_qpe, mask=mask_m[:, None], other=0.0)
+
+    # stage 1: compute scores with prefix
+    offs_n = tl.arange(0, BLOCK_N)
+
+    acc = tl.zeros([BLOCK_M, BLOCK_DV], dtype=tl.float32)
+    deno = tl.zeros([BLOCK_M], dtype=tl.float32)
+    e_max = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")
+
+
+    # stage 2: compute the trianlge part
+    cur_block_m_end = tl.minimum(cur_seq_len_extend, (cur_block_m + 1) * BLOCK_M)
+
+    offs_v = cur_seq_extend_start_contiguous * stride_vbs + cur_kv_head * stride_kh
+    # the offset to the current seq
+    v_block_ptr = tl.make_block_ptr(
+        base=V_Extend + offs_v,
+        shape=(cur_seq_len_extend, Lv),
+        strides=(stride_vbs, 1),
+        offsets=(0, 0),
+        block_shape=(BLOCK_N, BLOCK_DMODEL),
+        order=(1, 0),
+    )
+
+    offs_k = cur_seq_extend_start_contiguous * stride_kbs + cur_kv_head * stride_kh
+    # the offset to the current seq
+    k_block_ptr = tl.make_block_ptr(
+        base=K_Extend + offs_k,
+        shape=(Lv, cur_seq_len_extend),
+        strides=(1, stride_kbs),
+        offsets=(0, 0),
+        block_shape=(BLOCK_DMODEL, BLOCK_N),
+        order=(0, 1),
+    )
+
+    start_m = cur_block_m
+    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = tl.arange(0, BLOCK_N)
+    STAGE = 3
+    if STAGE & 1:
+        acc, deno, e_max = _attn_fwd_inner(acc, deno, e_max, q, k_block_ptr, v_block_ptr,  #
+                                        start_m, sm_scale, logit_cap, cur_block_m_end, mask_m,#
+                                        BLOCK_M, BLOCK_DMODEL, BLOCK_N,  #
+                                        4 - STAGE, offs_m, offs_n  #
+                                        )
+    # stage 2: on-band
+    if STAGE & 2:
+        # barrier makes it easier for compielr to schedule the
+        # two loops independently
+        tl.debug_barrier()
+        acc, deno, e_max = _attn_fwd_inner(acc, deno, e_max, q, k_block_ptr, v_block_ptr,  #
+                                        start_m, sm_scale, logit_cap, cur_block_m_end, mask_m,#
+                                        BLOCK_M, BLOCK_DMODEL, BLOCK_N,  #
+                                        2, offs_m, offs_n  #
+                                        )
+
+    
+
+
+    # Ragged inputs: out:[S, H, D]
+    offs_o = cur_seq_extend_start_contiguous * stride_obs + cur_head * stride_oh
+    # the offset to the current seq
+    o_block_ptr = tl.make_block_ptr(
+        base=O_Extend + offs_o,
+        shape=(cur_seq_len_extend, Lq),
+        strides=(stride_obs, 1),
+        offsets=(cur_block_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, BLOCK_DMODEL),
+        order=(1, 0),
+    )
+    #:[BLOCK_M, BLOCK_DMODEL]
+    o = acc / deno[:, None]
+    o = o.to(q.dtype)
+    #o = o.reshape(BLOCK_M, 1, BLOCK_DMODEL)
+    tl.store(o_block_ptr, o, boundary_check=(0, 1))
+
+       
 @triton.jit
 def _fwd_kernel(
     Q_Extend,
@@ -80,9 +310,9 @@ def _fwd_kernel(
     BLOCK_M: tl.constexpr,
     BLOCK_N: tl.constexpr,
 ):
-    cur_seq = tl.program_id(0)
-    cur_head = tl.program_id(1)
-    cur_block_m = tl.program_id(2)
+    cur_seq = tl.program_id(2)
+    cur_head = tl.program_id(0)
+    cur_block_m = tl.program_id(1)
     cur_kv_head = cur_head // kv_group_num
     kv_head_num = q_head_num // kv_group_num
 
@@ -142,74 +372,82 @@ def _fwd_kernel(
     deno = tl.zeros([BLOCK_M], dtype=tl.float32)
     e_max = tl.zeros([BLOCK_M], dtype=tl.float32) - float("inf")
 
-    for start_n in range(0, cur_seq_len_prefix, BLOCK_N):
-        start_n = tl.multiple_of(start_n, BLOCK_N)
-        mask_n = (start_n + offs_n) < cur_seq_len_prefix
-        offs_b_loc_prefix = cur_batch_req_idx * stride_req_to_tokens_b + (
-            cur_seq_prefix_start_in_loc + start_n + offs_n
-        )
-        offs_kv_loc = tl.load(Req_to_tokens + offs_b_loc_prefix, mask=mask_n, other=0)
+    # for start_n in range(0, cur_seq_len_prefix, BLOCK_N):
+    #     start_n = tl.multiple_of(start_n, BLOCK_N)
+    #     mask_n = (start_n + offs_n) < cur_seq_len_prefix
+    #     offs_b_loc_prefix = cur_batch_req_idx * stride_req_to_tokens_b + (
+    #         cur_seq_prefix_start_in_loc + start_n + offs_n
+    #     )
+    #     offs_kv_loc = tl.load(Req_to_tokens + offs_b_loc_prefix, mask=mask_n, other=0)
 
-        # load k in transposed way
-        offs_buf_k = (
-            offs_kv_loc[None, :] * stride_buf_kbs
-            + cur_kv_head * stride_buf_kh
-            + offs_d[:, None]
-        )
-        k = tl.load(
-            K_Buffer + offs_buf_k, mask=(mask_n[None, :]) & (mask_d[:, None]), other=0.0
-        )
+    #     # load k in transposed way
+    #     offs_buf_k = (
+    #         offs_kv_loc[None, :] * stride_buf_kbs
+    #         + cur_kv_head * stride_buf_kh
+    #         + offs_d[:, None]
+    #     )
+    #     k = tl.load(
+    #         K_Buffer + offs_buf_k, mask=(mask_n[None, :]) & (mask_d[:, None]), other=0.0
+    #     )
 
-        qk = tl.dot(q.to(k.dtype), k)
-        if BLOCK_DPE > 0:
-            offs_kpe = (
-                offs_kv_loc[None, :] * stride_buf_kbs
-                + cur_kv_head * stride_buf_kh
-                + offs_dpe[:, None]
-            )
-            kpe = tl.load(
-                K_Buffer + offs_kpe,
-                mask=mask_n[None, :],
-                other=0.0,
-            )
-            qk += tl.dot(qpe.to(kpe.dtype), kpe)
-        qk *= sm_scale
+    #     qk = tl.dot(q.to(k.dtype), k)
+    #     if BLOCK_DPE > 0:
+    #         offs_kpe = (
+    #             offs_kv_loc[None, :] * stride_buf_kbs
+    #             + cur_kv_head * stride_buf_kh
+    #             + offs_dpe[:, None]
+    #         )
+    #         kpe = tl.load(
+    #             K_Buffer + offs_kpe,
+    #             mask=mask_n[None, :],
+    #             other=0.0,
+    #         )
+    #         qk += tl.dot(qpe.to(kpe.dtype), kpe)
+    #     qk *= sm_scale
 
-        if logit_cap > 0:
-            qk = logit_cap * tanh(qk / logit_cap)
+    #     if logit_cap > 0:
+    #         qk = logit_cap * tanh(qk / logit_cap)
 
-        qk = tl.where(mask_m[:, None] & mask_n[None, :], qk, float("-inf"))
+    #     qk = tl.where(mask_m[:, None] & mask_n[None, :], qk, float("-inf"))
 
-        n_e_max = tl.maximum(tl.max(qk, 1), e_max)
-        re_scale = tl.exp(e_max - n_e_max)
-        p = tl.exp(qk - n_e_max[:, None])
-        deno = deno * re_scale + tl.sum(p, 1)
+    #     n_e_max = tl.maximum(tl.max(qk, 1), e_max)
+    #     re_scale = tl.exp(e_max - n_e_max)
+    #     p = tl.exp(qk - n_e_max[:, None])
+    #     deno = deno * re_scale + tl.sum(p, 1)
 
-        offs_buf_v = (
-            offs_kv_loc[:, None] * stride_buf_vbs
-            + cur_kv_head * stride_buf_vh
-            + offs_dv[None, :]
-        )
-        v = tl.load(
-            V_Buffer + offs_buf_v, mask=mask_n[:, None] & mask_dv[None, :], other=0.0
-        )
-        p = p.to(v.dtype)
-        acc = acc * re_scale[:, None] + tl.dot(p, v)
+    #     offs_buf_v = (
+    #         offs_kv_loc[:, None] * stride_buf_vbs
+    #         + cur_kv_head * stride_buf_vh
+    #         + offs_dv[None, :]
+    #     )
+    #     v = tl.load(
+    #         V_Buffer + offs_buf_v, mask=mask_n[:, None] & mask_dv[None, :], other=0.0
+    #     )
+    #     p = p.to(v.dtype)
+    #     acc = acc * re_scale[:, None] + tl.dot(p, v)
 
-        e_max = n_e_max
+    #     e_max = n_e_max
 
     # stage 2: compute the trianlge part
     cur_block_m_end = tl.minimum(cur_seq_len_extend, (cur_block_m + 1) * BLOCK_M)
     if use_block_ptr:
         offs_k = cur_seq_extend_start_contiguous * stride_kbs + cur_kv_head * stride_kh
         # the offset to the current seq
+        # k_block_ptr = tl.make_block_ptr(
+        #     base=K_Extend + offs_k,
+        #     shape=(cur_seq_len_extend, Lv),
+        #     strides=(stride_kbs, 1),
+        #     offsets=(0, 0),
+        #     block_shape=(BLOCK_N, BLOCK_DMODEL),
+        #     order=(1, 0),
+        # )
         k_block_ptr = tl.make_block_ptr(
             base=K_Extend + offs_k,
-            shape=(cur_seq_len_extend, Lv),
-            strides=(stride_kbs, 1),
+            shape=(Lv, cur_seq_len_extend),
+            strides=(1, stride_kbs),
             offsets=(0, 0),
-            block_shape=(BLOCK_N, BLOCK_DMODEL),
-            order=(1, 0),
+            block_shape=(BLOCK_DMODEL, BLOCK_N),
+            order=(0, 1),
         )
 
         offs_v = cur_seq_extend_start_contiguous * stride_vbs + cur_kv_head * stride_kh
@@ -240,8 +478,8 @@ def _fwd_kernel(
         else:
             # k:[BLOCK_N, 1, BLOCK_DMODEL]
             k = tl.load(k_block_ptr, boundary_check=(0, 1))
-            k = k.trans()
-            tl.advance(k_block_ptr, (BLOCK_N, 0))
+            #k = k.trans()
+            k_block_ptr = tl.advance(k_block_ptr, (0, BLOCK_N))
 
         qk = tl.dot(q, k, out_dtype=tl.float32)
         if BLOCK_DPE > 0:
@@ -285,7 +523,7 @@ def _fwd_kernel(
             )
         else:
             v = tl.load(v_block_ptr, boundary_check=(0, 1))
-            tl.advance(k_block_ptr, (BLOCK_N, 0))
+            v_block_ptr = tl.advance(v_block_ptr, (BLOCK_N, 0))
 
         p = p.to(v.dtype)
         acc = acc * re_scale[:, None] + tl.dot(p, v)
@@ -381,15 +619,19 @@ def extend_attention_fwd(
     sm_scale = sm_scale or 1.0 / (Lq**0.5)
     batch_size, head_num = b_seq_len.shape[0], q_extend.shape[1]
     kv_group_num = q_extend.shape[1] // k_extend.shape[1]
-    grid = (batch_size, head_num, triton.cdiv(max_len_extend, BLOCK_M))
+    #q/k/v: [seq_len, head_num, d_model] 
+    #
+    grid = (head_num, triton.cdiv(max_len_extend, BLOCK_M), batch_size)
+    #grid = (batch_size, head_num, triton.cdiv(max_len_extend, BLOCK_M))
     num_warps = 4 if Lk <= 64 else 8
     if is_xpu_available:
         num_warps = 8 if Lq == 64 else 16
     num_stages = 1
+    b_seq_len_extend = b_seq_len_extend.to(torch.int32)    
     #print("BLOCK_M", BLOCK_M, "BLOCK_N", BLOCK_N, "BLOCK_DMODEL", BLOCK_DMODEL, "BLOCK_DPE", BLOCK_DPE, "BLOCK_DV", BLOCK_DV)
     #print("Lq", Lq, "Lv", Lv, "kv_group_num", kv_group_num, "num_warps", num_warps, "num_stages", num_stages)
     #print(q_extend.shape, k_extend.shape, v_extend.shape, o_extend.shape, k_buffer.shape, v_buffer.shape, req_to_tokens.shape)
-    _fwd_kernel[grid](
+    _fwd_kernel2[grid](
         q_extend,
         k_extend,
         v_extend,
@@ -412,9 +654,9 @@ def extend_attention_fwd(
         o_extend.stride(0),
         o_extend.stride(1),
         k_buffer.stride(0),
-        k_buffer.stride(1),
+        k_buffer.stride(0),
         v_buffer.stride(0),
-        v_buffer.stride(1),
+        v_buffer.stride(0),
         req_to_tokens.stride(0),
         logit_cap=logit_cap,
         BLOCK_DMODEL=BLOCK_DMODEL,
@@ -424,13 +666,55 @@ def extend_attention_fwd(
         BLOCK_N=BLOCK_N,
         Lq=Lq,
         kv_group_num=kv_group_num,
-        use_block_ptr=USE_BLOCK_PTR,
+        use_block_ptr=1,
+        advanced_path=True,
         Lv=Lv,
         num_warps=num_warps,
         num_stages=num_stages,
-        grf_mode='large',
-        #advanced_path=True
+        grf_mode='large'
     )
+    # _fwd_kernel2[grid](
+    #     q_extend,
+    #     k_extend,
+    #     v_extend,
+    #     o_extend,
+    #     k_buffer,
+    #     v_buffer,
+    #     req_to_tokens,
+    #     b_req_idx,
+    #     b_seq_len,
+    #     b_start_loc_extend,
+    #     b_seq_len_extend,
+    #     sm_scale,
+    #     q_extend.shape[1],
+    #     q_extend.stride(0),
+    #     q_extend.stride(1),
+    #     k_extend.stride(0),
+    #     k_extend.stride(1),
+    #     v_extend.stride(0),
+    #     v_extend.stride(1),
+    #     o_extend.stride(0),
+    #     o_extend.stride(1),
+    #     k_buffer.stride(0),
+    #     k_buffer.stride(1),
+    #     v_buffer.stride(0),
+    #     v_buffer.stride(1),
+    #     req_to_tokens.stride(0),
+    #     logit_cap=logit_cap,
+    #     BLOCK_DMODEL=BLOCK_DMODEL,
+    #     BLOCK_DPE=BLOCK_DPE,
+    #     BLOCK_DV=BLOCK_DV,
+    #     BLOCK_M=BLOCK_M,
+    #     BLOCK_N=BLOCK_N,
+    #     Lq=Lq,
+    #     kv_group_num=kv_group_num,
+    #     use_block_ptr=1,
+    #     Lv=Lv,
+    #     num_warps=num_warps,
+    #     num_stages=num_stages,
+    #     #grf_mode='large',
+    #     #advanced_path=True
+    # )
 
 
 def redundant_attention(
